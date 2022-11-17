cce2f0fd026e7b43ac60e957b3dea0268162c9b4
Mikatech <mikael.vallenet@epitech.eu>
2022-11-17 01:05:36 +0100

    feat(sdk): proof of concept call API Starton from SDK

---
+++ sdk-starton/src/Main.java
@@ -0,0 +1,10 @@
+import initialization.Initializer;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Initializer client = new Initializer();
+    }
+}
---
+++ sdk-starton/src/initialization/Initializer.java
@@ -0,0 +1,24 @@
+package initialization;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class Initializer {
+
+    public Initializer() throws IOException {
+        URL url = new URL("https://api.starton.io/v2/wallet/");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+        con.setRequestProperty("x-api-key", "your api key");
+        BufferedReader br = null;
+        br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String strCurrentLine;
+        while ((strCurrentLine = br.readLine()) != null) {
+            System.out.println(strCurrentLine);
+        }
+    }
+}
